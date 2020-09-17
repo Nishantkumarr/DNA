a3139227012d57cdd01be3bece73793b9f7d359a
Nishant Kumar <mailittonishantkumar@gmail.com>
2020-09-17 20:54:36 +0530

    < form get data function dna>

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ app.config["SECRET_KEY"] = 'theXyz'
 def home():
     return render_template('index.html')
 
+@app.route('/dna', methods=['POST'])
+def dna():
+      dna_seq = request.form['dna_seq'])
+        #<your code for dna seq generation>
+       #return render_template(<page.html>)
+
+    
 if __name__ == "__main__":
     app.config["DEBUG"] = True
     app.run()
